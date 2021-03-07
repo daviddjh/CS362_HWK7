b8c52e5748af5a7d4b51b79f95909045d3949f73
David Headrick <david.headrick9@gmail.com>
2021-03-07 11:38:33 -0800

    Implemented fizzbuzz method

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+
+def fizzbuzz():
+  for i in range(1, 101):
+    if i % 5 == 0 and i % 3 == 0:
+      print("FizzBuzz")
+    elif i % 5 == 0:
+      print("Buzz")
+    elif i % 3 == 0:
+      print("Fizz")
+    else:
+      print(i)
+
+if __name__ == "__main__":
+  fizzbuzz()
