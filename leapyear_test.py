d190c86c245f4eba2f931ed6408034a4316ee30c
David Headrick <david.headrick9@gmail.com>
2021-03-07 12:14:41 -0800

    Wrote forth test, tests to see if year divisible by 400 is a leap year

--- leapyear_test.py
+++ leapyear_test.py
@@ -4,3 +4,4 @@ def test_leapyear():
   assert leapyear.isleapyear(1) == False
   assert leapyear.isleapyear(4) == True
   assert leapyear.isleapyear(100) == False
+  assert leapyear.isleapyear(400) == True
